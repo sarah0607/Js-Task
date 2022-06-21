46d1b5581875b79e2755bc7ab086672226052f30
Sarah <sarahossai6@gmail.com>
2022-06-21 17:19:02 -0500

    Js Task

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = "Sarah Ossai";
+var height = "6ft";
+var country = "Nigeria";
+
+document.write('Name:' + '  ' + name + '<br/>')
+document.write('Height:' + '  ' + height + '<br/>')
+document.write('Country:' + '  ' + country + '<br/>')
